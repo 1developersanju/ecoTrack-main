bb3269ac183e4c2b68f60287016dab8eb195ea29
s4njeev <s4njeev.av@gmail.com>
2023-12-15 11:35:37 +0530

    updated after running on localhost

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ from flask import json
 from random import randint
 from time import gmtime,time
 from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+from flask_cors import CORS
+
 
 
 # Flask app initialization
 
 app = Flask(__name__)
-
+CORS(app)
 #database configuration
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sanjuwings04:3gklnZaORj8Q@ep-soft-king-86780931.ap-southeast-1.aws.neon.tech/ecoTrack?sslmode=require'
@@ -354,6 +356,6 @@ def hourlydetails():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
-  
+   
